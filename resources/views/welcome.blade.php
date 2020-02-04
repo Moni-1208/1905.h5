94fe3e31989a65e23c1440c33c0cc397166389a8
baixue111 <baixue111@localhost.localdomain>
2020-02-05 03:39:39 +0800

    1

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    h5
+                    h5 嘻嘻
                 </div>
 
                 <div class="links">
